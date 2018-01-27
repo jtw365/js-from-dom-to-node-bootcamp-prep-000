f9809cd472034a6ac2aec9a25a068ff475e20731
Jonathan Williams <jtw365@live.missouristate.edu>
2018-01-27 18:59:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var dog = "thirsty"
- 
+var dog = "cuddly"
+
 if (dog === "hungry") {
   console.log("Refilling food bowl.")
+
 } else if (dog === "thirsty") {
   console.log("Refilling water bowl.")
+
+} else if (dog === "playful") {
+  console.log("Playing tug-of-war.")
+
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
+
 } else {
   console.log("Reading newspaper.")
 }
